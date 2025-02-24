f6144bb461bb5d9cde3fedca6139b67cb7dcbfd8
aksis7 <100067148+aksis7@users.noreply.github.com>
2025-02-24 21:15:05 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,7 @@
+$('#btn').click(function(event) {
+   
+    
+    let mail = $('#mail').val();
+    let password = $('#password').val();
+    console.log('mailVal:', mail, 'passVal:', password);
+  });
